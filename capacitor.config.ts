2bc6cd27352440a3df27a7728795dc7334d5224c
xt9779 <xt9779@rimac.com.pe>
2026-09-10 00:33:27 -0500

    correct capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.notechord.app',
   appName: 'NoteChord',
-  webDir: 'www'
+  webDir: 'www',
+  server: {
+    cleartext: true
+  }
 };
 
 export default config;
